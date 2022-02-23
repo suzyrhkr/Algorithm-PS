04b4cc0ea66ef23f0baa6602d27393f55a3fe365
Suzy Kwak <48341349+suzyrhkr@users.noreply.github.com>
2022-02-23 23:57:41 +0900

    add PS

---
+++ DFS-BFS/validate-binary-search-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root, low, high):
+            if root is None:
+                return True
+            
+            if root.val <= low or root.val >= high:
+                return False
+            
+            return dfs(root.left, low, root.val) and dfs(root.right, root.val, high)
+        
+        return dfs(root, -math.inf, math.inf)
