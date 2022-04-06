0938b8743f1927bb49198c984941737b12038b25
suzyrhkr <suzy030@naver.com>
2022-04-06 23:46:19 +0900

    Create Team-Notes

---
+++ Team-Notes/combinations-permutations.py
@@ -0,0 +1,25 @@
+'''combinations'''
+def combination(arr, n):
+    result = []
+    if n == 0:
+        return [[]]
+    for i in range(len(arr)):
+        elem = arr[i]
+        for rest in combination(arr[i + 1:], n - 1):
+            result.append([elem] + rest)
+    return result
+print(combination([0,1,2,3], 2))
+# [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]
+
+'''permutations'''
+def permutation(arr, n):
+    result = []
+    if n == 0:
+        return [[]]
+    for i in range(len(arr)):
+        elem = arr[i]
+        for rest in permutation(arr[:i] + arr[i+1:], n - 1):
+            result.append([elem] + rest)
+    return result
+print(permutation([0,1,2,3], 2))
+# [[0, 1], [0, 2], [0, 3], [1, 0], [1, 2], [1, 3], [2, 0], [2, 1], [2, 3], [3, 0], [3, 1], [3, 2]]
