019e80840ca115ca188653cabc027dd04b83481a
suzyrhkr <suzy030@naver.com>
2022-04-16 17:07:37 +0900

    add other solution

--- samsung-sw/마법사상어와복제.py
+++ samsung-sw/마법사상어와복제.py
@@ -1,5 +1,5 @@
+'''시간초과'''
 import sys, copy
-from collections import deque
 
 input = sys.stdin.readline
 m, s = map(int, input().split())
@@ -118,3 +118,89 @@ for i in range(s):
     duplicate(i)
     
 print(count_fishes())
+
+'''other solution'''
+import sys, copy
+
+sys.stdin = open('/Users/suzykwak/Desktop/input.txt','r')
+input = sys.stdin.readline
+m, s = map(int, input().split())
+fish = [list(map(int, input().split())) for _ in range(m)]
+space = [[[] for _ in range(4)] for _ in range(4)]
+
+for x, y, d in fish:
+    space[x - 1][y - 1].append(d - 1)
+
+shark = tuple(map(lambda x: int(x) - 1, input().split()))
+smell = [[0] * 4 for _ in range(4)]
+
+f_dx = [0, -1, -1, -1, 0, 1, 1, 1]
+f_dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+def move_fish():
+    res = [[[] for _ in range(4)] for _ in range(4)]
+    for x in range(4):
+        for y in range(4):
+            while temp[x][y]:
+                d = temp[x][y].pop()
+                for i in range(d, d - 8, -1):
+                    i %= 8
+                    nx, ny = x + f_dx[i], y + f_dy[i]
+                    if (nx, ny) != shark and 0 <= nx < 4 and 0 <= ny < 4 and not smell[nx][ny]:
+                        res[nx][ny].append(i)
+                        break
+                else:
+                    res[x][y].append(d)
+    return res
+
+def dfs(x, y, dep, cnt, visit):
+    global max_eat, shark, eat
+    if dep == 3:  
+        if max_eat < cnt:
+            max_eat = cnt
+            shark = (x, y)
+            eat = visit[:]
+        return
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            if (nx, ny) not in visit:  
+                visit.append((nx, ny))
+                dfs(nx, ny, dep + 1, cnt + len(temp[nx][ny]), visit)
+                visit.pop()
+            else:  
+                dfs(nx, ny, dep + 1, cnt, visit)
+
+for _ in range(s):
+    eat = list()
+    max_eat = -1
+    # 1. 모든 물고기 복제
+    temp = copy.deepcopy(space)
+    # 2. 물고기 이동
+    temp = move_fish()
+    # 3. 상어이동 - 백트래킹
+    dfs(shark[0], shark[1],0, 0, list())
+    # 4. 냄새 사라짐 
+    for i in range(4):
+        for j in range(4):
+            if smell[i][j]:
+                smell[i][j] -= 1
+    for x, y in eat:
+        if temp[x][y]:
+            temp[x][y] = []
+            smell[x][y] = 2   
+    # 5. 복제 마법
+    for i in range(4):
+        for j in range(4):
+            space[i][j] += temp[i][j]
+
+# 물고기 수 구하기 
+answer = 0
+for i in range(4):
+    for j in range(4):
+        answer += len(space[i][j])
+
+print(answer)
