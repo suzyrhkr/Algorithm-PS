e9894a4b5d0743e922f07553dab65e5ba6e1769b
suzyrhkr <suzy030@naver.com>
2022-04-28 22:18:43 +0900

    add other solution

--- samsung-sw/상어초등학교.py
+++ samsung-sw/상어초등학교.py
@@ -100,3 +100,79 @@ for st, f_sts in students_info:
     
 score = getScore(students_info, seats)
 print(score)
+
+#---other solution---
+# sort 함수 key = lambda를 이용하여 한 번에 정렬한 풀이
+# 1' 00
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+info = [list(map(int, input().split())) for _ in range(n*n)]
+space = [[0]*n for _ in range(n)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def calculate_score():
+    score = 0
+    for i in range(n):
+        for j in range(n):
+            for k in range(n**2):
+                if space[i][j] == info[k][0]:
+                    favorite = info[k][1:]
+                    break
+            cnt = 0
+            for idx in range(4):
+                nx, ny = i + dx[idx], j + dy[idx]
+                if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                    if space[nx][ny] in favorite:
+                        cnt += 1
+            if cnt == 0:
+                score += 0
+            else:
+                score += 10**(cnt-1)
+    return score
+
+def find_space(favorite):
+    arr = []
+    for i in range(n):
+        for j in range(n):
+            favorite_cnt, void_cnt = 0, 0
+            if 0 < space[i][j]:  # 이미 자리를 정한 학생이 있는 경우
+                continue
+            for idx in range(4): # 현재 위치에서 4 방향 탐색
+                nx, ny = i + dx[idx], j + dy[idx]
+                if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                    if space[nx][ny] == 0: # 비어있는 칸
+                        void_cnt += 1
+                    else: # 비어있지 않은 경우
+                        if space[nx][ny] in favorite: # 지금 칸에 있는 학생이 좋아하는 학생일 경우
+                            favorite_cnt += 1
+            arr.append([favorite_cnt, void_cnt, i, j]) # 좋아하는 학생 수 / 비어있는 칸 수
+    return arr
+
+for i, x in enumerate(info):
+    st = x[0]
+    favorite = [x[1], x[2], x[3], x[4]]
+
+    arr = find_space(favorite) 
+    arr.sort(key=lambda x:(-x[0], -x[1], x[2], x[3]))
+    st_x, st_y = arr[0][2], arr[0][3]
+
+    space[st_x][st_y] = st # 자리 배치
+
+print(calculate_score())
+
+"""
+상어 초등학교
+
+학생: 1~n**2번 
+인접 조건: 상하좌우
+
+1. 비어있는 칸 중 좋아하는 학생과 인접한 칸이 가장 많은 칸
+2. 1이 여러 개이면, 좋아하는 학생과 인접한 칸 중에서 비어있는 칸이 가장 많은 칸
+3. 2도 여러 개이면, 행, 열 번호 작은 칸 
+
+output: 자리 배치 후 만족도(좋아하는 학생 수에 따라 결정)의 총합
+"""
