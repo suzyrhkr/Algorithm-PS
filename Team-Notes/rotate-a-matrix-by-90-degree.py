49ce871abf33fd87f910fbdee476fa10581a94f7
suzyrhkr <suzy030@naver.com>
2022-04-28 20:43:29 +0900

    add other matrix rotation algorithm

--- Team-Notes/rotate-a-matrix-by-90-degree.py
+++ Team-Notes/rotate-a-matrix-by-90-degree.py
@@ -1,7 +1,7 @@
 n = 5
 space = [[(i*n)+j for j in range(n)] for i in range(n)]
 
-# clockwise 90°
+# 1. clockwise 90°
 def clockwise_rotate():
     rotated_space = [[0]*n for _ in range(n)]
     for i in range(n):
@@ -33,6 +33,35 @@ rotated_space:
 [24, 19, 14, 9, 4]
 '''
 
+# 2. clockwise 90°
+n, m = 4, 5
+space = [[(i*n)+j for j in range(m)] for i in range(n)]
+
+for row in space:
+    print(row)
+print()
+
+rotated_space = list(map(list, zip(*space[::-1])))
+
+for row in rotated_space:
+    print(row)
+
+'''
+space:
+[0, 1, 2, 3, 4]
+[4, 5, 6, 7, 8]
+[8, 9, 10, 11, 12]
+[12, 13, 14, 15, 16]
+
+rotated_space:
+[12, 8, 4, 0]
+[13, 9, 5, 1]
+[14, 10, 6, 2]
+[15, 11, 7, 3]
+[16, 12, 8, 4]
+'''
+
+# ------------------------------------------------------
 n = 5
 space = [[(i*n)+j for j in range(n)] for i in range(n)]
 
